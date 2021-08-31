8ca27dfeab2bc589577c54933f41cbb131315d9f
Isaac Novaes <isaac.novaes@hotmail.com>
2021-08-31 20:00:57 +0200

    Update files. Last block and header need to be finished

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const burger = document.querySelector(".burger");
-
-burger.addEventListener("click" , () => {
-    const links = document.querySelector(".nav-links");
-    burger.classList.toggle("active-burger");
-    links.classList.toggle("active-burger-content");
-    Array.from(links.children).forEach((element, index) => {
-        element.style.animationDelay = `${index/5 + 0.5}s`;
-    });
-})
+const burger = document.querySelector(".burger"),
+	links = document.querySelector(".nav-links");
+burger.addEventListener("click", () => {
+	if (burger) {
+		burger.classList.toggle("active-burger");
+		links.classList.toggle("active-burger-content");
+		document.body.classList.toggle("overflow-hide");
+		Array.from(links.children).forEach((element, index) => {
+			element.style.animationDelay = `${index / 5 + 0.5}s`;
+		});
+	}
+});
+//Make sure the nav is restyled
+window.addEventListener("resize", () => {
+	if (window.innerWidth >= 800) {
+		burger.classList.remove("active-burger");
+		links.classList.remove("active-burger-content");
+	}
+});
